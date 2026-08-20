01ebca9de1c1c54cc0691fd692674ba57196fc8e
YI YONGSINH <sinh3784@gmail.com>
2026-08-20 22:36:57 +0700

    model again

--- app/Models/Attendances.php
+++ app/Models/Attendances.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Attendances extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'date',
+        'time_in',
+        'time_out',
+        'status',
+        'user_id',
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
