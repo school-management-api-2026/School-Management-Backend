0c8a814211671af29685af4b9df25301e8ff009a
YI YONGSINH <sinh3784@gmail.com>
2026-08-26 06:44:24 +0700

    crud user and auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -11,7 +13,11 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+        return response()->json([
+            'message' => 'Get all users',
+            'data' => $users,
+        ],200);
     }
 
     /**
@@ -27,15 +33,36 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'nullable|string|max:150|unique:users,username',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:6',
+            'phone' => 'nullable|string|unique:users,phone',
+            'gender' => 'nullable|string|max:30',
+            'date_of_birth' => 'nullable|date',
+            'role_id' => 'required|exists:roles,id'
+        ]);
+
+        $validated['password'] = bcrypt($validated['password']);
+
+        $user = User::create($validated);
+
+        return response()->json([
+            'message'=>'Created user successfully.',
+            'data'=>$user,
+        ],201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(User $users)
     {
-        //
+        return response()->json([
+            'message' => 'Get all users',
+            'data' => $users,
+        ],200);
     }
 
     /**
@@ -43,22 +70,66 @@ class UserController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, User $user)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'username' => 'nullable|string|max:150|unique:users,username,' . $user->id,
+            'email' => 'required|string|email|max:255|unique:users,email,' . $user->id,
+            'password' => 'nullable|string|min:6',
+            'phone' => 'nullable|string|unique:users,phone,' . $user->id,
+            'gender' => 'nullable|string|max:30',
+            'date_of_birth' => 'nullable|date',
+            'role_id' => 'required|exists:roles,id'
+        ]);
+
+        if (!empty($validated['password'])) {
+            if (Hash::check($request->password, $user->password)) {
+                unset($validated['password']); // បើ Password ដូចគ្នា គឺដកចេញមិនបាច់ update
+            } else {
+                $validated['password'] = bcrypt($validated['password']); // បើប្តូរថ្មី ធ្វើការ Hash
+            }
+        } else {
+            unset($validated['password']);
+        }
+
+        $hasChange = false;
+        foreach($validated as $key => $value) {
+            if($user->{$key} != $value){
+                $hasChange = true;
+                break;
+            }
+        }
+
+        if(!$hasChange) {
+            return response()->json([
+                'message' => 'Nothing user update',
+            ]);
+        }
+
+        $user->update($validated);
+
+        return response()->json([
+            'message' => 'Update user successfuly',
+            'data' => $user,
+        ],200);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return response()->json([
+            'message' => 'Delete user successfully'
+        ],200);
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Roles;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,10 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Roles::create(['name' => 'Admin']);
+        \App\Models\Roles::create(['name' => 'Teacher']);
+        \App\Models\Roles::create(['name' => 'Library_staff']);
+        \App\Models\Roles::create(['name' => 'Student']);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'superadmin',
+            'username' => 'superadmin',
+            'email' => 'superadmin@gmail.com',
+            'password' => bcrypt('superadmin'),
+            'phone' => "1234556667",
+            'role_id' => 1
+        ]);
     }
 }
